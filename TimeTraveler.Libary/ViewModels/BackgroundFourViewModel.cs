9e0241c6d91a9b4e231ec855be3b549ff137c77a
ImnotMiiikey <994587699@qq.com>
2024-12-18 17:20:11 +0800

    2

---
+++ TimeTraveler.Libary/ViewModels/BackgroundFourViewModel.cs
@@ -0,0 +1,16 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.Messaging;
+using TimeTraveler.Libary.Services;
+
+namespace TimeTraveler.Libary.ViewModels;
+
+public partial class BackgroundFourViewModel : ViewModelBase
+{
+    public BackgroundFourViewModel() { }
+
+    [RelayCommand]
+    public void GoToGameView()
+    {
+        WeakReferenceMessenger.Default.Send<object, string>(1, "OnForwardNavigation");
+    }
+}
