1696479667779b52f61061f72b5aafe96a7294a6
ImnotMiiikey <994587699@qq.com>
2024-12-23 17:00:02 +0800

    Revert "Revert "1""
    This reverts commit 988a6295e118cd2c7e1a4a4d01da30270e8200b3.

---
+++ TimeTraveler/Views/GameFourView.axaml.cs
@@ -0,0 +1,39 @@
+﻿using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using Avalonia.Media;
+using TimeTraveler.Libary.ViewModels;
+using TimeTraveler.Services;
+
+namespace TimeTraveler.Views
+{
+    public partial class GameFourView : UserControl
+    {
+        public GameFourView()
+        {
+            InitializeComponent();
+            this.DataContext = new GameFourViewModel(new ResultVerifyFourService()); // 根据实际的验证服务传入
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+        private void Button_PointerEntered_1(object? sender, Avalonia.Input.PointerEventArgs e)
+        {
+            var button = sender as Button;
+            if (button != null)
+            {
+                button.Background = Brushes.AliceBlue;
+            }
+        }
+
+        private void Button_PointerExited_2(object? sender, Avalonia.Input.PointerEventArgs e)
+        {
+            var button = sender as Button;
+            if (button != null)
+            {
+                button.Background = new SolidColorBrush(Color.Parse("#E9E5D9"));
+            }
+        }
+    }
+}
