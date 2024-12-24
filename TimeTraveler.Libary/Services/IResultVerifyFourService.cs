df447f10ff39a4f4d116485c3c4191de1f899769
Stevensteven22 <2317603849@qq.com>
2024-12-24 16:47:11 +0800

    提交

---
+++ TimeTraveler.Libary/Services/IResultVerifyFourService.cs
@@ -0,0 +1,14 @@
+﻿namespace TimeTraveler.Libary.Services;
+
+public interface IResultVerifyFourService
+{
+    public string VerifyOrder(List<string> selectedOrder, List<string> correctOrder);
+
+    public void RestartGame(ref string gameStatus, ref int currentOrder, ref string button1Color,
+        ref string button2Color, ref string button3Color,
+        ref string button4Color, ref string button5Color, ref string button6Color, ref string button7Color,
+        ref string button8Color,
+        ref string button9Color, List<int> selectedOrder);
+
+    public void ShowResultImage(ref bool imageSuccess);
+}
