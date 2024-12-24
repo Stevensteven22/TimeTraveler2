1dce395e2d3533682b1d776db30ba9fb23a5dbf9
Stevensteven22 <2317603849@qq.com>
2024-12-24 13:19:20 +0800

    提交合并

--- TimeTraveler/Services/ElementalService.cs
+++ TimeTraveler/Services/ElementalService.cs
@@ -98,6 +98,7 @@ public class ElementalService : IElementalService
                 switch (resultModel.ResultElementType)
                 {
                     case ElementType.FireElemental:
+<<<<<<< HEAD
                         existingBuff.Value2 =
                             existingBuff.Value2 * (1.0d + (resultModel.ImprovedValue2 / 100.0d));
                         await _buffStorage.SaveBuffsAsync(existingBuff);
@@ -118,6 +119,28 @@ public class ElementalService : IElementalService
                         break;
                     case ElementType.WindElemental:
                         existingBuff.Value1 = existingBuff.Value1 + resultModel.ImprovedValue1;
+=======
+                       existingBuff.Value2 +=
+                            existingBuff.Value2 * (resultModel.ImprovedValue2 / 100.0d);
+                        await _buffStorage.SaveBuffsAsync(existingBuff);
+                        break;
+                    case ElementType.IceElemental:
+                        existingBuff.Value2 +=
+                            existingBuff.Value2 * (resultModel.ImprovedValue2 / 100.0d);
+                        await _buffStorage.SaveBuffsAsync(existingBuff);
+                        break;
+                    case ElementType.RockElemental:
+                       existingBuff.Value2 +=
+                            existingBuff.Value2 * (resultModel.ImprovedValue2 / 100.0d);
+                        await _buffStorage.SaveBuffsAsync(existingBuff);
+                        break;
+                    case ElementType.ThunderElemental:
+                        existingBuff.Value1 +=  resultModel.ImprovedValue1;
+                        await _buffStorage.SaveBuffsAsync(existingBuff);
+                        break;
+                    case ElementType.WindElemental:
+                        existingBuff.Value1 +=  resultModel.ImprovedValue1;
+>>>>>>> 05c51f3bb8a2c1dfa350f1b0bbc6032c38973f27
                         await _buffStorage.SaveBuffsAsync(existingBuff);
                         break;
                 }
